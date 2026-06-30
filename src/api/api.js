1f69b39f91a2be1fabf874bdd03fb1c037e3f089
Roy Kirorei <ratlab87@gmail.com>
2026-06-30 17:08:07 +0300

    Attach JWT token to API requests

--- src/api/api.js
+++ src/api/api.js
@@ -10,20 +10,14 @@ const api = axios.create({
 
 // Request logger
 api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   console.log("🚀 API:", config.method?.toUpperCase(), config.url);
+
   return config;
 });
-
-// Response logger
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error(
-      "❌ API ERROR:",
-      error.response?.data || error.message
-    );
-    return Promise.reject(error);
-  }
-);
-
 export default api;
